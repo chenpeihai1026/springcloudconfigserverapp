15c3ee81aa2363ab3c311c7e5ec3f8d2b2e21d5c
Peihai Chen <Peihai_Chen@ad.infosys.com>
2017-08-04 10:13:43 +0800

    add config client

---
+++ springconfigserverclient/src/main/java/com/mk/springcloud/config/client/app/SpringconfigserverclientApplication.java
@@ -0,0 +1,12 @@
+package com.mk.springcloud.config.client.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringconfigserverclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringconfigserverclientApplication.class, args);
+	}
+}
