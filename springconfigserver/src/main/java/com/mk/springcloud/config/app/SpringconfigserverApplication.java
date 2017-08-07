393298379a636e1c53b74d06097c9b55aa54e2bd
Peihai Chen <Peihai_Chen@ad.infosys.com>
2017-08-07 14:22:30 +0800

    add spring eureka

--- springconfigserver/src/main/java/com/mk/springcloud/config/app/SpringconfigserverApplication.java
+++ springconfigserver/src/main/java/com/mk/springcloud/config/app/SpringconfigserverApplication.java
@@ -1,15 +1,14 @@
 package com.mk.springcloud.config.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@EnableAutoConfiguration
-@RestController
+
 @EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
 public class SpringconfigserverApplication {
 
 	public static void main(String[] args) {
--- springconfigserverclient/src/main/java/com/mk/springcloud/config/client/app/SpringconfigserverclientApplication.java
+++ springconfigserverclient/src/main/java/com/mk/springcloud/config/client/app/SpringconfigserverclientApplication.java
@@ -2,8 +2,10 @@ package com.mk.springcloud.config.client.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringconfigserverclientApplication {
 
 	public static void main(String[] args) {
