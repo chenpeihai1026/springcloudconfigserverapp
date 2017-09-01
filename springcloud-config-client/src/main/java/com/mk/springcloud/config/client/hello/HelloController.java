3fc4a065b178314ddb8b169a191668fb296d1070
Peihai Chen <Peihai_Chen@ad.infosys.com>
2017-09-01 10:28:33 +0800

    add changed

--- springcloud-config-client/src/main/java/com/mk/springcloud/config/client/hello/HelloController.java
+++ springcloud-config-client/src/main/java/com/mk/springcloud/config/client/hello/HelloController.java
@@ -1,4 +1,4 @@
-package com.mk.springcloud.config.client;
+package com.mk.springcloud.config.client.hello;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中
+@RefreshScope
 public class HelloController {
-
+	
 	@Value("${config.name}")
 	private String hello;
-
+	
 	@RequestMapping("/hello")
-	public String from() {
-		return this.hello;
-	}
+    public String from() {
+        return this.hello;
+    }
 }
